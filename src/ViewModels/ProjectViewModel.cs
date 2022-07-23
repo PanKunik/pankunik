36bc5fd6c2c967fec329e808fd20b1fc1115324c
PanKunik <patryk.kunicki@pm.me>
2022-07-23 18:53:13 +0200

    Adding links to repositories and downloads for projects.

--- src/ViewModels/ProjectViewModel.cs
+++ src/ViewModels/ProjectViewModel.cs
@@ -1,4 +1,12 @@
 namespace Blazor.ViewModels
 {
-    public record ProjectViewModel (string Title, string Description, string ImagePath, string WindowsDownloadLink, string LinuxDownloadLink, string MacDownloadLink, string GithubLink, string[] Tags);
+    public record ProjectViewModel (
+        string Title,
+        string Description,
+        string ImagePath,
+        string WindowsDownloadLink,
+        string LinuxDownloadLink,
+        string MacDownloadLink,
+        string GithubLink,
+        string[] Tags);
 }
