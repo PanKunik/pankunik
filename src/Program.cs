99c6d8c0927eee62974c53cd57e4869f10deb620
PanKunik <patryk.kunicki@pm.me>
2022-07-23 19:56:24 +0200

    Adding WebOptimizer.Core for minifying and bundling css files

--- src/Program.cs
+++ src/Program.cs
@@ -7,8 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddWebOptimizer(pipeline =>
+{
+    pipeline.AddCssBundle("/css/bundle.css", sourceFiles: new string[] { "/wwwroot/css/*.css", "/obj/Release/net6.0/scopedcss/**/*.css" }).UseContentRoot();
+    pipeline.MinifyCssFiles("/css/bundle.css");
+});
 builder.Services.AddScoped<IImageService, ImageService>();
-
 
 var app = builder.Build();
 
@@ -22,6 +26,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseWebOptimizer();
 app.UseStaticFiles();
 
 app.UseRouting();
